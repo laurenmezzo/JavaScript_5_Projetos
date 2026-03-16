31ce6e31cdde90befc97c0de2f7dac8171ea030d
Lauren A.C. <mingufofo@gmail.com>
2026-03-15 22:07:33 -0300

    Máxima redução da calculadora para finalização e debugging

--- 4_Calculadora Complexa/script.js
+++ 4_Calculadora Complexa/script.js
@@ -2,54 +2,40 @@
 
 let display = document.getElementById("display");
 let ans = "";
-let count = 0;
 
 function numAppendToDisplay(input){
     display.value += input;
     display.placeholder +=input;
-    count++;
 
     console.log(display.placeholder);
 };
 
+function aPiToDisplay(){
+    display.value += Math.PI.toFixed(6);
+    display.placeholder +=Math.PI.toFixed(6);
+
+    console.log(display.placeholder);
+}
+
 function opAppendToDisplay(input){
     display.value += input;
     display.placeholder +=input;
-    count++;
 
     console.log(display.placeholder);
 };
 
 function percent(){
     display.value += "%";
-    display.placeholder += display.placeholder + "/100";
-    count++;
+    display.placeholder += "/100";
+    calculate();
     
     console.log(display.placeholder);
 };
-
-function expAppendToDisplay(){
-    display.value += "^";
-    display.placeholder += "**";
-    count++;
-    count++;
-
-    console.log(count);
-    console.log(display.placeholder);
-};
-function sqrtAppendToDisplay(){
-    display.placeholder += "**(1/2)";
-    display.value = eval(display.placeholder);
-    let count = display.value.length;
-    
-    console.log(display.placeholder);
-}
 
 function clearAll(){
     display.value = "";
     display.placeholder="";
     let ans="0";
-    let count=0;
     
     console.log(display.placeholder);
 };
@@ -57,10 +43,6 @@ function clearAll(){
 function clearLast(){
     display.value = display.value.slice(0, -1);
     display.placeholder = display.placeholder.slice(0, -1);
-    console.log(display.placeholder.at(count-2));
-    if (display.placeholder.at(count-2) === "*" ){
-        display.placeholder = display.placeholder.slice(0, -1);
-    };
 
     console.log(display.placeholder);
 };
@@ -73,9 +55,6 @@ function calculate(){
     catch(error){
         display.value = "Erro"
     };
-    let ans= display.value;
-    let lastInput= "calc";
-    let count = display.placeholder.length;
 
     console.log(display.placeholder);
 };
