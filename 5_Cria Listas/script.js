b1e1f9708029e2e046940e4bec33c4ff2fc04046
Lauren A.C. <mingufofo@gmail.com>
2026-03-16 21:04:26 -0300

    Finalização do cria listas

--- 5_Cria Listas/script.js
+++ 5_Cria Listas/script.js
@@ -1,6 +1,21 @@
-let input = document.getElementById("input").value;
+let userInp = document.getElementById("input").value;
+let count = 0;
 
 function createStuff(){
-    let input = document.getElementById("input").value;
-    
+    let userInp = document.getElementById("input").value;
+    if (userInp === ""){
+        document.getElementById("span").innerHTML = "Por favor, insira alguma informação."
+    } else{
+        document.getElementById("span").innerHTML = ""
+
+        let argument= "<li id='" + count + "'><p>" + userInp + "</p><button onclick='selfDelete(" + count + ")'>X</button>";
+        document.getElementById("groupStuff").insertAdjacentHTML("beforeend", argument);
+        count++;
+    };
+};
+
+function selfDelete(input){
+    console.log(input);
+    let parent = document.getElementById(input);
+    parent.remove();
 };
